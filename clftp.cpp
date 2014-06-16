3ad0d3b77afd690d00461ecad44dbb9051164768
Oren Samuel <oren.samuel@mail.huji.ac.il>
2014-06-16 12:16:49 +0300

    arranging client code

--- clftp.cpp
+++ clftp.cpp
@@ -1,75 +1,70 @@
 //********************************************************************
-//This program created by Netanel Zakay for OS course, 2013-2014, HUJI
+// clftp.cpp - Client side for a simple file-transfer protocol.
+// This program created by Netanel Zakay for OS course, 2013-2014, HUJI
+// Edited by Yoni Herzog & Oren Samuel
 //********************************************************************
 
 
 
 //********************************* INCLUDES **********************************
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <netdb.h>
-#include <unistd.h>
+#include "ftp.h"
+
 #include <fstream>
 
 using namespace std;
 
-
 //********************************* VARIABLES *********************************
-
-#define SUCCESS 0
-#define ERROR -1
-#define EXIT_CODE 1
-#define BUFFER_SIZE 1024
 
 #define PORT_PARA_INDX 1
 #define HOST_PARA_INDX 2
 #define FILE_NAME_PARA_INDX 3
 #define OUT_FILE_NAME_PARA_INDX 4
 
-int _sPort;
-int _serverSocket;
-int _nameSize;
-int _fileSize;
-char* _fileToTransfer;
-char* _fileToSave;
-
+int server_port;
+int server_socket;
+int name_size;
+int file_size;
+char* filename_to_transfer;
+char* filename_to_save;
 
 struct sockaddr_in serv_addr;
 struct hostent *server = NULL;
 
+//****************************** HELPER FUNCTIONS ****************************
 
-//****************************** HELPERS FUNCTIONS ****************************
-
-void error(string errorMessage){
+void error(string errorMessage)
+{
 	cerr << errorMessage;
 	exit(EXIT_CODE);
 }
 
-int getFileSize(ifstream &ifs) {
-	long begin,end;
+/**
+ * Returns the size of the given file.
+ */
+int get_file_size(ifstream &ifs)
+{
+	long begin, end;
 	begin = ifs.tellg();
-	ifs.seekg (0, ios::end);
+	ifs.seekg(0, ios::end);
 	end = ifs.tellg();
 	ifs.seekg(ios::beg);
-	return end-begin;
+	return end - begin;
 }
 
-void sendAllBuffer (char* buffer, int bufferSize){
-	int bytesSent=0;
-	int sent=1;
+void send_buffer(char* buf, int buf_size)
+{
+	int bytes_sent = 0;
+	int sent = 1;
 
-	while (bytesSent<bufferSize){
-		sent = send(_serverSocket, buffer + bytesSent, bufferSize- bytesSent,0);
-		if (sent== ERROR)
-			error ("ERROR: reciving data");
-		bytesSent += sent;
+	while (bytes_sent < buf_size)
+	{
+		sent = send(server_socket, buf + bytes_sent, buf_size - bytes_sent, 0);
+		if (sent== FAIL)
+		{
+			error ("ERROR: receiving data");
+		}
+		bytes_sent += sent;
 	}
 }
 
@@ -77,16 +72,16 @@ void sendFileContent(ifstream& ifs){
 	char* buffer = (char*) malloc(BUFFER_SIZE);
 	if (buffer==NULL)
 		error ("ERROR: malloc error.");
-	int toSend=_fileSize;
+	int toSend=file_size;
 
 	while (toSend>BUFFER_SIZE){
 		ifs.read(buffer,BUFFER_SIZE);
-		sendAllBuffer(buffer , BUFFER_SIZE);
+		send_buffer(buffer , BUFFER_SIZE);
 		toSend-=BUFFER_SIZE;
 	}
 	if (toSend!=0){
 		ifs.read(buffer,toSend);
-		sendAllBuffer(buffer , toSend);
+		send_buffer(buffer , toSend);
 	}
 	
 	free (buffer);
@@ -98,54 +93,54 @@ void sendFileContent(ifstream& ifs){
 
 int main(int argc, char** argv){
 
-	//get the port and host
-	_sPort = atoi(argv[PORT_PARA_INDX]);
+	// Get the port and host
+	server_port = atoi(argv[PORT_PARA_INDX]);
 	server = gethostbyname(argv[HOST_PARA_INDX]);
 	string tmp;
 
 	//get the (local) file name
 	tmp = argv[FILE_NAME_PARA_INDX]; 
-	_fileToTransfer = (char*)malloc(tmp.size()+1);
-	if (_fileToTransfer==NULL)
+	filename_to_transfer = (char*)malloc(tmp.size()+1);
+	if (filename_to_transfer==NULL)
 		error ("ERROR: malloc error.");
-	memcpy (_fileToTransfer, tmp.c_str(), tmp.size()+1);
+	memcpy (filename_to_transfer, tmp.c_str(), tmp.size()+1);
 	tmp = argv[OUT_FILE_NAME_PARA_INDX]; 
 
 	//get the file to save name
-	_nameSize=tmp.length();
-	_fileToSave = (char*)malloc(tmp.size()+1);
-	if (_fileToSave==NULL)
+	name_size=tmp.length();
+	filename_to_save = (char*)malloc(tmp.size()+1);
+	if (filename_to_save==NULL)
 		error ("ERROR: malloc error.");
-	memcpy (_fileToSave, tmp.c_str(), tmp.size()+1);
+	memcpy (filename_to_save, tmp.c_str(), tmp.size()+1);
 
 	//create a socket:
-	_serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr, server->h_length);
-	serv_addr.sin_port = htons(_sPort);
+	serv_addr.sin_port = htons(server_port);
 
 	//open the file and get it's size:
-	ifstream ifs (_fileToTransfer, ios::in);
+	ifstream ifs (filename_to_transfer, ios::in);
 	if (ifs == NULL)
 		error("ERROR: open file");
 
 	//connect to the server.
-	if (connect(_serverSocket,((struct sockaddr*)&serv_addr),sizeof(serv_addr)) < 0)//connecting to server
+	if (connect(server_socket,((struct sockaddr*)&serv_addr),sizeof(serv_addr)) < 0)//connecting to server
 		error("ERROR connecting");
 
-	_fileSize = getFileSize(ifs);
+	file_size = get_file_size(ifs);
 
 	//send all the information
-	sendAllBuffer((char*) &_nameSize , sizeof(int));
-	sendAllBuffer(_fileToSave, _nameSize+1);
-	sendAllBuffer ((char*) &_fileSize, sizeof (int));
+	send_buffer((char*) &name_size , sizeof(int));
+	send_buffer(filename_to_save, name_size+1);
+	send_buffer ((char*) &file_size, sizeof (int));
 	sendFileContent(ifs);
 
 	//closing
-	free (_fileToSave);
-	free (_fileToTransfer);
-	close(_serverSocket);
+	free (filename_to_save);
+	free (filename_to_transfer);
+	close(server_socket);
 	ifs.close();
 
 	return SUCCESS;
